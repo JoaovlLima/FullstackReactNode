67aa429db75a521685edd8ce6409b2ca2c19b381
JoaovlLima <jl949615@gmail.com>
2024-08-29 16:43:41 -0300

    29/08

--- biblioteca-front/src/components/ListaLivros.js
+++ biblioteca-front/src/components/ListaLivros.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../App.css'; // Importa o CSS no componente
+import '../App.css'; 
 
 function ListaLivros() {
   const [livros, setLivros] = useState([]);
